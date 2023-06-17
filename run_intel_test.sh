b37ac89884d6cf9ee4b75aafcd1685d770459d80
notalol1con <zbl183905063@gmail.com>
2023-06-18 00:26:19 +1000

    Added test on intel mac

---
+++ run_intel_test.sh
@@ -0,0 +1,23 @@
+echo "array_type,time,element_size" > "./data/${1}_results.csv"
+for i in {1..20}
+do
+    for SIZE in {120,248,504,1016,2040}
+    do
+        gcc -g -O3 -DSIZE=$SIZE basic_access.c -o basic
+        for R in {2,3,4}
+        do
+            touch ${1}.c
+            make clean_base target=${1} > /dev/null
+            make "target=${1}" "SIZE=$(($SIZE + 8))" "R=${R}" > /dev/null
+        done
+        ./basic
+        ./"${1}Mono"
+        ./"${1}Sliced"
+        ./"${1}SQ"
+        ./"${1}HAT"
+        ./"${1}MS_2"
+        ./"${1}MS_3"
+        ./"${1}MS_4"
+        make clear_target target=${1} > /dev/null
+    done
+done
